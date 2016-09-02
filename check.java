2e91b268b4bfeaef00abcdc2c9574be2fe56b6cc
meenaarul <meenaarul.be@gmail.com>
2016-09-02 11:45:40 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,20 @@
+class check
+{
+ public static void main(String arg[])
+   {
+   Scanner in=new Scanner(System.in);
+   System.out.print("enter the number :");
+    int n;
+n=in.nextInt();
+   
+    if(n/2==0)
+    {
+    System.out.print("given number is even number:");
+}
+else
+  {
+  System.out.print("given number is odd number:");
+     
+ }
+}
+}
